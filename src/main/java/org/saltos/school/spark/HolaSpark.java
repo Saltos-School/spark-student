4581d730e4cb0c43378a725143db0d19efc80eb4
Carlos Saltos <csaltos@gmail.com>
2024-10-14 06:41:49 -0300

    Hola Spark

--- src/main/java/org/saltos/school/spark/HolaSpark.java
+++ src/main/java/org/saltos/school/spark/HolaSpark.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 public class HolaSpark {
 
+    static String cambiarAMayusculas(String s) {
+        return s.toUpperCase();
+    }
+
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
@@ -16,15 +20,30 @@ public class HolaSpark {
                 .config("spark.master", "local")
                 .getOrCreate();
         JavaSparkContext jsc = JavaSparkContext.fromSparkContext(spark.sparkContext());
+        jsc.setLogLevel("WARN");
         List<String> nombresEnJava = Arrays.asList("Carlos", "Pepe", "Anna", "Paul");
         JavaRDD<String> nombresEnSpark = jsc.parallelize(nombresEnJava);
         System.out.println("Nombres en Spark original");
         nombresEnSpark.foreach(nombre -> System.out.println(nombre));
         JavaRDD<String> nombresEnMayusculasRDD = nombresEnSpark.map(nombre -> nombre.toUpperCase());
+        JavaRDD<String> nombresEnMayusculasRDD2 = nombresEnSpark.map(String::toUpperCase);
+        JavaRDD<String> nombresEnMayusculasRDD3 =nombresEnSpark.map(nombre -> {
+           String nombreEnMayusculas = nombre.toUpperCase();
+           return nombreEnMayusculas;
+        });
+        JavaRDD<String> nombresEnMayusculasRDD4 = nombresEnSpark.map(HolaSpark::cambiarAMayusculas);
         System.out.println("Nombres en mayusculas");
-        nombresEnMayusculasRDD.foreach(nombre -> System.out.println(nombre));
+        nombresEnMayusculasRDD.foreach(nombre -> System.out.println("[" + nombre + "]"));
         System.out.println("Nombres en Spark original");
         nombresEnSpark.foreach(nombre -> System.out.println(nombre));
+
+        JavaRDD<Integer> numerosRDD = nombresEnSpark.map(nombre -> nombre.length());
+        JavaRDD<Integer> numerosRDD2 = nombresEnSpark.map(String::length);
+
+        numerosRDD.foreach(numero -> System.out.println("numero: " + numero));
+        long numeroDeItems = numerosRDD.count();
+        System.out.println("El numero de items es: " + numeroDeItems);
+
     }
 
 }
