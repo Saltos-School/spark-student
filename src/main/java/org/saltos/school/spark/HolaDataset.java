c657f9fee2fb3b5141c23fca98886e6aeb329d55
Carlos Saltos <csaltos@gmail.com>
2024-10-16 06:53:53 -0300

    Hola Dataset

--- src/main/java/org/saltos/school/spark/HolaDataset.java
+++ src/main/java/org/saltos/school/spark/HolaDataset.java
@@ -1,9 +1,7 @@
 package org.saltos.school.spark;
 
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 public class HolaDataset {
 
@@ -19,6 +17,23 @@ public class HolaDataset {
         Dataset<Row> employeesDF = spark.read().json("src/main/resources/employees.json");
         employeesDF.printSchema();
         employeesDF.show();
+
+        System.out.println("Empleados con salario mayor a 3500:");
+        employeesDF.filter((Row fila) -> {
+            Long salario = fila.getLong(1);
+            return salario > 3500;
+        }).show();
+
+        System.out.println("Empleados con salario mayor a 3500:");
+        employeesDF.filter("salary > 3500").show();
+
+        Encoder<EmpleadoBean> encoderEmpleadoBean = Encoders.bean(EmpleadoBean.class);
+        Dataset<EmpleadoBean> employeesDS = employeesDF.as(encoderEmpleadoBean);
+        employeesDS.printSchema();
+        employeesDS.show();
+
+        System.out.println("Empleados con salario mayor a 3500:");
+        employeesDS.filter((EmpleadoBean empleado) -> empleado.getSalary() > 3500).show();
 
         jsc.close();
         spark.close();
